ae607411b1f2e65bac82488373f6770d8a63ca48
Gontzal Monasterio <go.monasterio@gmail.com>
2020-10-15 13:07:17 +0200

    save

--- download-prof.py
+++ download-prof.py
@@ -2,7 +2,8 @@
 
 import click
 import requests
-from conf import BASE_URL, IMG_DIR
+from scipy.stats import mode
+from conf import BASE_URL
 from src import dbops
 
 @click.command()
@@ -11,10 +12,11 @@ def dowload_prof():
     stages = dbops.fetch_stages(project=["result", "profile"])
     
     for stage in stages:
-        # Get winner's cluster and set output path
-        winner = stage["result"][0]
-        cluster = dbops.fetch_rider(winner)["cluster"]
-        path = f"{IMG_DIR}/cluster{cluster}"
+        # Get mode of top 10 rider's clusters and set output path
+        result = stage["result"]
+        clusters = [dbops.fetch_rider(rider)["cluster"] for rider in result]
+        cluster = mode(clusters).mode[0]
+        path = f"profiles/cluster{cluster}"
     
         # Download profile
         url = f"{BASE_URL}/{stage['profile']}"
